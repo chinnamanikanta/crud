d4fa0d88f62458cef5a04dde8e7e9800ac3e9f38
manikanta <manikantakondapalli944@gmail.com>
2021-11-11 15:43:32 -0800

    code writting

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React,{useEffect} from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { loadUsers } from '../redux/Actions'
+
+
 const Dashboard = () => {
     const users = useSelector((state) => state.users)
+const dispatch = useDispatch()
+    useEffect(() => {
+        loadUsers(dispatch)
+
+
+
+
+    })
 
     console.log(users)
     return (
@@ -9,7 +20,20 @@ const Dashboard = () => {
           <table>
               <tbody>
                   
-                  mani
+                {
+                    users.map((user) => {
+                        return (
+                            <tr key = {user.id}>
+                                <td>{user.name}</td>
+                                <td>{user.gmail}</td>
+                                <td>{user.contact}</td>
+                                <td>{user.address}</td>
+                            </tr>
+                            
+                            )
+
+                    })
+                }
               </tbody>
           </table>
         </div>
