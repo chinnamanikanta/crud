d5b049116df9075386f987da2f12b8c4aade5acc
manikanta <manikantakondapalli944@gmail.com>
2021-11-11 16:19:33 -0800

    changed local host

--- src/redux/Actions.js
+++ src/redux/Actions.js
@@ -12,7 +12,7 @@ export const loadUsers = () => {
     return (dispatch) => {
 
         axios.get('http://localhost:5000/user').then((res) => {
-console.log(res)
+console.log("res", res)
             dispatch(getUsers(res))
         })
         .catch(error => console.log((error)))
